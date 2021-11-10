b63c64f40b0c5e9699cc2f69bd90a7c6e4b147bb
Olivier Kaloudoff <kaloudov@yandex.com>
2021-11-10 19:55:32 +0100

    ports/mpv:  - add flags to ffmpeg headers/libs

--- ports/mpv/build.sh
+++ ports/mpv/build.sh
@@ -9,33 +9,39 @@
 #
 
 
-set -e
+set -ex
 source ../../lib.sh
 DISTVER="mpv"
 DISTSUFFIX="git"
-GIT_TAG=v0.34.0
+#GIT_TAG=v0.34.0
+GIT_TAG=9628edede001e7af0dcf487a3d27d909ad4e87e3~1
+ZLIB_DIR=zlib-1.2.11
+LIBFFMPEG_DIR="ffmpeg-4.2.2"
 
-TASK=fetch
+
+TASK=build
+#TASK=fetch
 
 DISTFILES="https://github.com/mpv-player/$DISTVER.$DISTSUFFIX"
 #UNPACKCOMD="tar -xzf"
 
 package_init "$@"
-CONFIGURE_CMD="./configure
-                --host=$PBHOSTARCH
-                --build=$PBBUILDARCH 
-                --target=$PBTARGETARCH 
-                --prefix=$PREFIX 
-                CC=$PBTARGETARCH-gcc 
-                CFLAGS=\"-I$PWD\"
-                "
+CONFIGURE_CMD=" ./TOOLS/old-configure 
+		--cc=$PBTARGETARCH-gcc 
+  		--extra-cflags=\"-I$PWD  -I$ARCHIVEDIR/$ZLIB_DIR/$PREFIX/include -I$ARCHIVEDIR/$LIBFFMPEG_DIR/$PREFIX/include\"
+  		--extra-libs=\" -lz -liconv \" 
+  		--extra-ldflags=\"-L$ARCHIVEDIR/$LIBFFMPEG_DIR/$PREFIX/lib\"
+		--disable-libass 
+		--disable-ffmpeg 
+                --prefix=$PREFIX "
+
 
 if [ "$TASK" == "fetch" ]
 then
   cd "$WORKROOT"
-  git clone $DISTFILES --depth 1 --single-branch --branch $GIT_TAG
-  #cd "$WORKDIR"
-  #git checkout $GIT_TAG
+  git clone $DISTFILES --single-branch --branch master
+  cd "$WORKDIR"
+  git checkout $GIT_TAG
   TASK=patch
 fi
 
